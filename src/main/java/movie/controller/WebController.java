8ce1948ea894532c20c74d6055c86f7ea6fee065
Adam Serck <you@example.com>
2020-11-17 20:15:54 -0600

    1_sprint_us4-4 Rental Query
    Added onto the rental repository by adding mapping to the rental registration page, and post mapping for confirming the entry

--- src/main/java/movie/controller/WebController.java
+++ src/main/java/movie/controller/WebController.java
@@ -1,18 +1,22 @@
 package movie.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import movie.beans.Member;
+import movie.beans.Rental;
 import movie.repository.MemberRepository;
+import movie.repository.RentalRepository;
 
 @Controller
 public class WebController {
 	@Autowired
 	MemberRepository repo;
+	RentalRepository rentRepo;
 	
 	@GetMapping({"/", "/memberRegistration"})
 	public String addNewMember(Model model) {
@@ -25,6 +29,21 @@ public class WebController {
 	public String addNewMember(@ModelAttribute Member m, Model model) {
 		repo.save(m);
 		return "home";
+	}
+	
+	
+	@GetMapping( "/rentalRegistration")
+	public String addNewRental(Model model) {
+		Rental r = new Rental();
+		model.addAttribute("newRental", r);
+	return "addRental";
+	}
+	
+	@PostMapping("/rentalRegistration")
+	public String addNewRental(@ModelAttribute Rental r, Model model) {
+		rentRepo.save(r);
+		model.addAttribute("rental", r);
+		return "confirmRental";
 	}
 	
 	@GetMapping("/homePage")
