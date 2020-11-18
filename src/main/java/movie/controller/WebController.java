2ca895dd6851135cf2faba4d1b106cfa3e3470f5
Juovitus <noahmhoss@gmail.com>
2020-11-18 13:24:17 -0600

    Edit/Update/Delete
    Added in edit/update/delete for Members

--- src/main/java/movie/controller/WebController.java
+++ src/main/java/movie/controller/WebController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import movie.beans.Member;
 import movie.beans.Rental;
 import movie.repository.MemberRepository;
@@ -59,4 +61,26 @@ public class WebController {
 	public String memberHomePage() {
 	return "home";
 	}
+	
+	@GetMapping("/edit/{id}")
+	public String showUpdateMember(@PathVariable("id") long id, Model model) {
+		Member m = repo.findById(id).orElse(null);
+		System.out.println("ITEM TO EDIT: " + m.toString());
+		model.addAttribute("newMember", m);
+		return "registration";
+	}
+
+	@PostMapping("/update/{id}")
+	public String reviseMember(Member m, Model model) {
+		repo.save(m);
+		return viewAllMembers(model);
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteMember(@PathVariable("id") long id, Model model) {
+		Member m = repo.findById(id).orElse(null);
+	    repo.delete(m);
+	    return viewAllMembers(model);
+	}
+	
 }
