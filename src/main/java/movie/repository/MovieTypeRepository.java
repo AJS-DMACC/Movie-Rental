4bc53c8bf33d1fc5e897a9993d5c55f3049b5ae0
Md Ahmed <mahmed4@dmacc.edu>
2020-12-06 21:30:59 -0600

    added movie type class repo and UI

---
+++ src/main/java/movie/repository/MovieTypeRepository.java
@@ -0,0 +1,9 @@
+package movie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import movie.beans.MovieType;
+@Repository
+public interface MovieTypeRepository extends JpaRepository<MovieType, Integer>{
+
+}
